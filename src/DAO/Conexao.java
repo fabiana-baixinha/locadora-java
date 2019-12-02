2d70854b6f9080dc1c7838c25adae0674ebfc1e6
User <User@DESKTOP-KLRTJHD>
2019-12-02 08:48:06 -0300

    conexao

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -6,10 +6,30 @@
 package DAO;
 import java.sql.*;
 import javax.swing.JOptionPane;
-public class Conexao {
-    
 
-         
-     }
+public class Conexao {
+    public static Connection AbrirConexao() throws ClassNotFoundException{
+      Connection con = null;
+      try {
+          Class.forName("com.mysql.jdbc.Drive");
+          String url = "jdbc:mysql://localhost/locadora";
+          con = DriverManager.getConnection(url,"root", "123");
+     
+      } catch (Exception e){
+          JOptionPane.showMessageDialog(null, "Erro na conexão com o banco",
+             "Video locadora",JOptionPane.ERROR_MESSAGE);
+            e.getMessage();
+            
+      }
+       return con;
+       }
+    public static void FecharConexao(Connection con) {
+        try {
+            con.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
  
 
